06d95fde560fa63b8c5788d5c1fee0164d2c918e
Alkeshkumar94 <alkeshkumar949@gmail.com>
2024-07-31 10:00:03 +0530

    change

--- src/components/UserDetail.js
+++ src/components/UserDetail.js
@@ -10,16 +10,16 @@ function UserDetail() {
     // Simulate a fetch call
     setTimeout(() => {
       const users=[
-        { id: 1, Name: 'Leanne Graham', Username:"Bret",Email:"john@122",Phone:12332,Website:"john.com" },
-        { id: 2, Name: 'Ervin Howell', Username:"Antonette",Email:"bran@122",Phone:12332,Website:"bran.com" },
-        { id: 3, Name: 'Clementine Bauch', Username:"Samantha",Email:"Ram@122",Phone:12332,Website:"Ram.com" },
-        { id: 4, Name: 'Patricia Lebsack', Username:"Shyam",Email:"Shyam@122",Phone:12332,Website:"Shyam.com" },
-        { id: 5, Name: 'Chelsey Dietrich', Username:"Rahul",Email:"Rahul@122",Phone:12332,Website:"Rahul.com" },
-        { id: 6, Name: 'Mrs Dennis Schulist', Username:"Alkesh",Email:"Alkesh@122",Phone:12332,Website:"Alkesh.com" },
-        { id: 8, Name: 'Kurtis Weissnat', Username:"Tushar",Email:"Tushar@122",Phone:12332,Website:"Tushar.com" },
-        { id: 9, Name: 'Nicholas Runolfsdottir V', Username:"Tushar",Email:"Tushar@122",Phone:12332,Website:"Tushar.com" },
-        { id: 10, Name: 'Glenna Reichert', Username:"Tushar",Email:"Tushar@122",Phone:12332,Website:"Tushar.com" },
-        { id: 11, Name: 'Clementina DuBuque', Username:"Tushar",Email:"Tushar@122",Phone:12332,Website:"Tushar.com" },
+        { id: 1, Name: 'Leanne Graham', Username:"Bret",Email:"john@122.cnd",Phone:12332,Website:"john.com" },
+        { id: 2, Name: 'Ervin Howell', Username:"Antonette",Email:"bran@122.ewe",Phone:12332,Website:"bran.com" },
+        { id: 3, Name: 'Clementine Bauch', Username:"Samantha",Email:"Ram@122.wew",Phone:12332,Website:"Ram.com" },
+        { id: 4, Name: 'Patricia Lebsack', Username:"Shyam",Email:"Shyam@122.www",Phone:12332,Website:"Shyam.com" },
+        { id: 5, Name: 'Chelsey Dietrich', Username:"Rahul",Email:"Rahul@122.wqq",Phone:12332,Website:"Rahul.com" },
+        { id: 6, Name: 'Mrs Dennis Schulist', Username:"Alkesh",Email:"Alkesh@122.sxs",Phone:12332,Website:"Alkesh.com" },
+        { id: 8, Name: 'Kurtis Weissnat', Username:"Tushar",Email:"Tushar@122.eww",Phone:12332,Website:"Tushar.com" },
+        { id: 9, Name: 'Nicholas Runolfsdottir V', Username:"Tushar",Email:"Tushar@122.dwa",Phone:12332,Website:"Tushar.com" },
+        { id: 10, Name: 'Glenna Reichert', Username:"Tushar",Email:"Tushar@122.dss",Phone:12332,Website:"Tushar.com" },
+        { id: 11, Name: 'Clementina DuBuque', Username:"Tushar",Email:"Tushar@122.xss",Phone:12332,Website:"Tushar.com" },
       
       ]
       const user = users.find(user => user.id === parseInt(id));
